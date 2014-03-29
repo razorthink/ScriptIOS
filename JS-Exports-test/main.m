3d6ba1e5233d2c10b1eaa270c2362ccf8d43d793
Sandeep S Kumar <sanygeek@gmail.com>
2014-03-29 23:47:48 +0530

    Initial Commit

---
+++ JS-Exports-test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JS-Exports-test
+//
+//  Created by Sandeep S Kumar on 29/03/14.
+//  Copyright (c) 2014 Razorthink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ JS-Exports-testTests/JS_Exports_testTests.m
@@ -0,0 +1,34 @@
+//
+//  JS_Exports_testTests.m
+//  JS-Exports-testTests
+//
+//  Created by Sandeep S Kumar on 29/03/14.
+//  Copyright (c) 2014 Razorthink. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JS_Exports_testTests : XCTestCase
+
+@end
+
+@implementation JS_Exports_testTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
